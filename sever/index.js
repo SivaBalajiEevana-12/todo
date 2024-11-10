162e6302f692b870b17d5dd8d2447685df63313b
SivaBalajiEevana-12 <eevansivabalaji@gmail.com>
2024-11-10 14:28:05 +0530

    ignoring the node modules

---
+++ sever/index.js
@@ -0,0 +1,7 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const cors=require('cors');
+const app=express();
+app.listen(5000,()=>{
+    console.log(`htpp://localhost:5000`);
+})
